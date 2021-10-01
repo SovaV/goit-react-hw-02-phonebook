d03e400be4ffe9bb8901ea9ed0db407703f9e1b9
SovaV <sova.volodymyr95@gmail.com>
2021-10-02 00:18:25 +0300

    sd

--- src/component/ContactElement/ContactElement.js
+++ src/component/ContactElement/ContactElement.js
@@ -1,9 +1,13 @@
-const ContactsEl = ({ id, name, number }) => (
-  <div>
-    <p key={id}>
-      {name}: {number}
-    </p>
+import PropTypes from 'prop-types';
+import ce from './ContactElement.module.css';
+const ContactsEl = ({ name, number }) => (
+  <div className={ce.box}>
+    <p>{name}</p>
+    <p>: {number}</p>
   </div>
 );
-
+ContactsEl.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+};
 export default ContactsEl;
